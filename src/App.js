90b3ee4c5d0d17628a7f72071127f5dc90e16d0d
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-01-18 20:41:39 +0200

    feat: open github in new tab

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
           <RepeatingList repeatingItems={this.state.repeatingItems}></RepeatingList>
         </div>
         <footer>
-          <a href="https://github.com/Zeitverschwender/Spaced-Repetition">
+          <a href="https://github.com/Zeitverschwender/Spaced-Repetition" target="_blank" rel="noopener noreferrer">
             Github
           </a>
         </footer>
