c7e5b17de52652900f737728d49272aaea5163a4
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-01-20 18:19:21 +0200

    refactor: remove unused property

--- src/models/repeatingitem.js
+++ src/models/repeatingitem.js
@@ -1,8 +1,7 @@
 export class RepeatingItem {
-  constructor(_id, title, streak, description) {
+  constructor(_id, title, description) {
     this._id = _id;
     this.title = title;
-    this.streak = streak;
     this.description = description;
   }
 
