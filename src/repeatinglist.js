38be6d1d18d4c3afb96f1a77cd65c21521d163b8
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-01-20 18:18:47 +0200

    feat: item loading animation

--- src/repeatinglist.js
+++ src/repeatinglist.js
@@ -2,11 +2,21 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 import { RepeatingItem } from "./repeatingitem";
+import { RepeatingItem as RItem } from "./models/repeatingitem";
 
+const LOADING_ITEM = new RItem(0, "Loading..", "");
 export class RepeatingList extends Component {
   render() {
     return (
       <div className="repeating-list">
+        {this.props.repeatingItems.length === 0 && (
+          <div className="loading-item">
+            <RepeatingItem
+              key={LOADING_ITEM._id}
+              item={LOADING_ITEM}
+            ></RepeatingItem>
+          </div>
+        )}
         {this.props.repeatingItems.map((item) => (
           <RepeatingItem key={item._id} item={item}></RepeatingItem>
         ))}
