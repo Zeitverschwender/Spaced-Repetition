f43764b6ff192c08f217fb4e3922796fdd550fb4
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-01-20 15:39:03 +0200

    feat: Repeating Item UI

--- src/repeatingitem.js
+++ src/repeatingitem.js
@@ -4,7 +4,25 @@ import PropTypes from "prop-types";
 
 export class RepeatingItem extends Component {
   render() {
-    return <React.Fragment>Content</React.Fragment>;
+    return (
+      <div className="repeating-item">
+        <div className="streak-part">
+          <span className="streak">Streak</span>
+          <span className="streak-count">43</span>
+        </div>
+        <div className="vertical-sep"></div>
+        <div className="item-details">
+          <span className="item-title">{this.props.item.title}</span>
+          <div className="item-icons">
+            <span className="material-icons">notifications</span>
+            <span class="material-icons">subject</span>
+          </div>
+        </div>
+        <div className="item-end">
+          <span>after 1 hr</span>
+        </div>
+      </div>
+    );
   }
 }
 
