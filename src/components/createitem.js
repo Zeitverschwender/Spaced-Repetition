0329903102bdec6b912918d5d7600726e287283f
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-01-21 14:56:25 +0200

    feat: Create Item

---
+++ src/components/createitem.js
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+
+import './createitem.scss';
+
+export class CreateItem extends Component {
+  state = {
+    isCreateItemShown: false,
+    newItem: {
+      title: "",
+      interval: "",
+    },
+  };
+
+  constructor(props) {
+    super(props);
+    this.createItemTextbox = React.createRef();
+  }
+
+  createItemOnClick = (e) => {
+    e.preventDefault();
+    this.setState(
+      {
+        isCreateItemShown: true,
+      },
+      () => this.createItemTextbox.current.focus()
+    );
+  };
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.isCreateItemShown && (
+          <div className="create-item">
+            <input
+              type="text"
+              ref={this.createItemTextbox}
+              placeholder="Item title"
+              onChange={(e) =>
+                this.setState({
+                  newItem: { ...this.state.newItem, title: e.target.value },
+                })
+              }
+            />
+            <select
+              defaultValue=""
+              onChange={(e) =>
+                this.setState({
+                  newItem: {
+                    ...this.state.newItem,
+                    interval: e.target.value,
+                  },
+                })
+              }
+            >
+              <option value="" disabled hidden>
+                Interval
+              </option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            <div className="create-item-buttons">
+              <button
+                className="create"
+                disabled={
+                  !(this.state.newItem.title && this.state.newItem.interval)
+                }
+              >
+                Create
+              </button>
+              <button className="more-options">More Options</button>
+              <button className="cancel">
+                <span className="material-icons">close</span>
+              </button>
+            </div>
+          </div>
+        )}
+        {!this.state.isCreateItemShown && (
+          <div className="create-item-button" onClick={this.createItemOnClick}>
+            <span className="material-icons">add</span>Create Item
+          </div>
+        )}
+      </React.Fragment>
+    );
+  }
+}
+
+export default CreateItem;
--- src/repeatinglist.js
+++ src/repeatinglist.js
@@ -3,13 +3,10 @@ import PropTypes from "prop-types";
 
 import { RepeatingItem } from "./repeatingitem";
 import { RepeatingItem as RItem } from "./models/repeatingitem";
+import { CreateItem } from "./components/createitem";
 
 const LOADING_ITEM = new RItem(0, "Loading..", "");
 export class RepeatingList extends Component {
-  createItemOnClick(e) {
-    e.preventDefault();
-    alert('create item clicked');
-  }
   render() {
     return (
       <div className="repeating-list">
@@ -24,9 +21,7 @@ export class RepeatingList extends Component {
         {this.props.repeatingItems.map((item) => (
           <RepeatingItem key={item._id} item={item}></RepeatingItem>
         ))}
-        <div className="create-item" onClick={this.createItemOnClick}>
-          <span className="material-icons">add</span>Create Item
-        </div>
+        <CreateItem></CreateItem>
       </div>
     );
   }
