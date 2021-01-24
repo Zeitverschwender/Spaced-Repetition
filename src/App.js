894a8123df4a59a1744d74dd629d3cbd6b82b6df
Ahmed Mohamed Abdelhamid <ahmedms.ftw@gmail.com>
2021-01-24 15:24:22 +0200

    feat: added basic router with switching and links. added About Us link to footer with image. added link to repeating lists to headet title. added package.json file to get the installed dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 
 import {RepeatingItem} from "./models/repeatingitem";
 
@@ -8,6 +9,8 @@ import RepeatingList from "./repeatinglist";
 import "./App.scss";
 import AboutPage from "./AboutPage";
 import githubIcon from "./assets/images/github.svg";
+import homeIcon from "./assets/images/home.svg";
+import aboutIcon from "./assets/images/info.svg";
 
 class App extends Component {
   state = {
@@ -30,18 +33,31 @@ class App extends Component {
   render() {
     return (
       <div className="main-wrapper">
+        <BrowserRouter>
         <header>
+        <Link to="/">
           <span className="title">Spaced Repetition</span>
+          </Link>
         </header>
-        <AboutPage></AboutPage>
-        {/* <div className="content">
-          <RepeatingList repeatingItems={this.state.repeatingItems}></RepeatingList>
-        </div> */}
-        <footer>
+          <switch>
+            <Route exact path="/">
+              <div className="content">
+                <RepeatingList repeatingItems={this.state.repeatingItems}></RepeatingList>
+              </div>
+            </Route>
+            <Route exact path="/About">
+              <AboutPage></AboutPage>
+            </Route>
+          </switch>
+          <footer>
           <a href="https://github.com/Zeitverschwender/Spaced-Repetition" target="_blank" rel="noopener noreferrer">
             <img src={githubIcon}></img>Github
           </a>
+          <Link to="/About">
+          <img src={aboutIcon}></img><span>About us</span>
+          </Link>
         </footer>
+        </BrowserRouter>
       </div>
     );
   }
