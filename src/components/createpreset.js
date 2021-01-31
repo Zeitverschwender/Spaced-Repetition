b4810d728e90ad0514b93083ca5c69e780e85474
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-01-31 13:40:49 +0200

    feat: create preset items

--- src/components/createpreset.js
+++ src/components/createpreset.js
@@ -5,6 +5,7 @@ import "./createpreset.scss";
 
 function CreatePreset(props) {
   const [title, setTitle] = useState("");
+  const [interval, setInterval] = useState([[1, "day"]]);
   return (
     <div className="overaly-back" onClick={props.hideMe}>
       <div
@@ -30,6 +31,35 @@ function CreatePreset(props) {
             maxLength="128"
             onChange={(e) => setTitle(e.target.value)}
           />
+          <h3>You will get a notification</h3>
+          <div className="create-preset-values">
+            {interval.map(([number, unit], i) => {
+              return (
+                <div key={i} className="create-preset-value">
+                  <span
+                    className="material-icons preset-reorder-item"
+                    title="Re-order"
+                  >
+                    drag_indicator
+                  </span>
+                  <span className="preset-value-start-text">
+                    {i === 0 ? "First After" : "Then After"}
+                  </span>
+                  <input type="number" defaultValue={number} min="1" max="30" />
+                  <select defaultValue={unit}>
+                    <option value="day">DAY(S)</option>
+                    <option value="month">MONTH(S)</option>
+                  </select>
+                  <span
+                    className="material-icons preset-remove-item"
+                    title="Remove Item"
+                  >
+                    clear
+                  </span>
+                </div>
+              );
+            })}
+          </div>
         </div>
         <div className="create-preset-footer">
           <div className="horizontal-sep" />
