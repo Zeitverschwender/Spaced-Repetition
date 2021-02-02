b9c4da62ed4ff4561ecbfe10e2ed1faa3ccdae81
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-02-02 21:05:25 +0200

    refactor: interval selection componenet

--- src/components/createitem.js
+++ src/components/createitem.js
@@ -1,37 +1,25 @@
-import React, { useState, useRef, useEffect, useContext } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 
 import "./createitem.scss";
-import CreateInterval from "./createinterval";
 
-import { NotificationQueueContext } from "./notificationqueue";
-import Backend from "../services/backend";
+import IntervalSelection from "./intervalselection";
 
 export default function CreateItem(props) {
   const [isCreateItemShown, setIsCreateItemShown] = useState(false);
   const [newItem, setNewItem] = useState({});
   const [isAllOptionsShown, setIsAllOptionsShown] = useState(false);
-  const [isCreateIntervalShown, setIsCreateIntervalShown] = useState(false);
-  const [intervals, setIntervals] = useState([]);
 
-  const createItemTextbox = useRef(null);
-
-  const createNotification = useContext(NotificationQueueContext);
-  useEffect(() => {
-    const _backend = new Backend();
-    const callback = (data) => setIntervals([...intervals, ...data]);
-    _backend.getDefaultIntervals(callback, createNotification);
-    _backend.getIntervals(callback, createNotification);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const titleTextbox = useRef(null);
 
   useEffect(() => {
     if (isCreateItemShown) {
-      createItemTextbox.current.focus();
+      titleTextbox.current.focus();
     }
   }, [isCreateItemShown]);
 
   const exitCreation = () => {
+    titleTextbox.current.value = "";
     setIsCreateItemShown(false);
     setNewItem({});
     setIsAllOptionsShown(false);
@@ -51,92 +39,71 @@ export default function CreateItem(props) {
   };
   return (
     <React.Fragment>
-      {isCreateIntervalShown && (
-        <CreateInterval
-          hideMe={() => setIsCreateIntervalShown(false)}
-          onAddNewInterval={(newInterval) =>
-            setIntervals([...intervals, newInterval])
+      <div
+        className="create-item"
+        style={{ display: isCreateItemShown ? "" : "none" }}
+      >
+        <input
+          type="text"
+          className="input-stylized"
+          ref={titleTextbox}
+          placeholder="Item title"
+          onChange={(e) => setNewItem({ ...newItem, title: e.target.value })}
+        />
+        <IntervalSelection
+          onIntervalChange={(interval) => setNewItem({ ...newItem, interval })}
+          className="interval"
+          selectClassName={
+            "create-item-select" +
+            (newItem.interval ? "" : " select-placeholder")
           }
-        ></CreateInterval>
-      )}
-      {isCreateItemShown && (
-        <div className="create-item">
-          <input
-            type="text"
-            ref={createItemTextbox}
-            placeholder="Item title"
-            onChange={(e) => setNewItem({ ...newItem, title: e.target.value })}
-          />
-          <div className="interval">
-            <select
-              defaultValue=""
-              className={newItem.interval ? "" : "select-placeholder"}
-              onChange={(e) => {
-                let value = e.target.value;
-                if (value === "create-item") {
-                  value = "";
-                  e.target.value = "";
-                  setIsCreateIntervalShown(true);
-                }
-                setNewItem({ ...newItem, interval: e.target.value });
-              }}
-            >
-              <option value="" disabled hidden>
-                Interval
-              </option>
-              {intervals.map((item) => (
-                <option key={item._id} value={item._id}>
-                  {item.title} ({item.days.toString()})
-                </option>
-              ))}
-              <option value="create-item">Create Item ...</option>
-            </select>
-          </div>
-          {isAllOptionsShown && (
-            <div className="all-options">
-              <textarea
-                placeholder="description"
+        />
+        {isAllOptionsShown && (
+          <div className="all-options">
+            <textarea
+              className="input-stylized"
+              placeholder="description"
+              onChange={(e) =>
+                setNewItem({ ...newItem, description: e.target.value })
+              }
+            />
+            <div className="is-notifications-on">
+              <label htmlFor="isNotificationsOn">Notifications</label>
+              <input
+                type="checkbox"
+                defaultChecked
+                id="isNotificationsOn"
                 onChange={(e) =>
-                  setNewItem({ ...newItem, description: e.target.value })
+                  setNewItem({
+                    ...newItem,
+                    isNotificationsOn: e.target.checked,
+                  })
                 }
               />
-              <div className="is-notifications-on">
-                <label htmlFor="isNotificationsOn">Notifications</label>
-                <input
-                  type="checkbox"
-                  defaultChecked
-                  id="isNotificationsOn"
-                  onChange={(e) =>
-                    setNewItem({
-                      ...newItem,
-                      isNotificationsOn: e.target.checked,
-                    })
-                  }
-                />
-              </div>
             </div>
-          )}
-          <div className="create-item-buttons">
-            <button
-              className="create"
-              disabled={!(newItem.title && newItem.interval)}
-              onClick={createOnClick}
-            >
-              Create
-            </button>
-            <button className="more-options" onClick={toggleAllOptions}>
-              <span className="material-icons" title="Toggle Full Options">
-                tune
-              </span>
-            </button>
-            <button className="cancel" onClick={cancelOnClick}>
-              <span className="material-icons" title="Close">
-                close
-              </span>
-            </button>
           </div>
+        )}
+        <div className="create-item-buttons">
+          <button
+            className="create"
+            disabled={!(newItem.title && newItem.interval)}
+            onClick={createOnClick}
+          >
+            Create
+          </button>
+          <button className="more-options" onClick={toggleAllOptions}>
+            <span className="material-icons" title="Toggle Full Options">
+              tune
+            </span>
+          </button>
+          <button className="cancel" onClick={cancelOnClick}>
+            <span className="material-icons" title="Close">
+              close
+            </span>
+          </button>
         </div>
-      )}
+      </div>
+
       {!isCreateItemShown && (
         <div className="create-item-button" onClick={createItemOnClick}>
           <span className="material-icons">add</span>Create Item
---
+++ src/components/intervalselection.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect, useContext } from "react";
+import PropTypes from "prop-types";
+
+import CreateInterval from "./createinterval";
+
+import { NotificationQueueContext } from "./notificationqueue";
+import Backend from "../services/backend";
+
+function IntervalSelection(props) {
+  const [isCreateIntervalShown, setIsCreateIntervalShown] = useState(false);
+  const [intervals, setIntervals] = useState([]);
+
+  const createNotification = useContext(NotificationQueueContext);
+  useEffect(() => {
+    const _backend = new Backend();
+    const callback = (data) => setIntervals([...intervals, ...data]);
+    _backend.getDefaultIntervals(callback, createNotification);
+    _backend.getIntervals(callback, createNotification);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <React.Fragment>
+      {isCreateIntervalShown && (
+        <CreateInterval
+          hideMe={() => setIsCreateIntervalShown(false)}
+          onAddNewInterval={(newInterval) =>
+            setIntervals([...intervals, newInterval])
+          }
+        ></CreateInterval>
+      )}
+      <div className={props.className}>
+        <select
+          defaultValue=""
+          className={props.selectClassName}
+          onChange={(e) => {
+            let value = e.target.value;
+            if (value === "create-item") {
+              value = "";
+              e.target.value = "";
+              setIsCreateIntervalShown(true);
+            }
+            props.onIntervalChange(e.target.value);
+          }}
+        >
+          <option value="" disabled hidden>
+            Interval
+          </option>
+          {intervals.map((item) => (
+            <option key={item._id} value={item._id}>
+              {item.title} ({item.days.toString()})
+            </option>
+          ))}
+          <option value="create-item">Create Item ...</option>
+        </select>
+      </div>
+    </React.Fragment>
+  );
+}
+
+IntervalSelection.propTypes = {
+  onIntervalChange: PropTypes.func.isRequired,
+};
+
+export default IntervalSelection;
